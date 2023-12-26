7820bfad5d767b48e80553098d9a9151708853ad
vineeth <vineethmvinoy@gmail.com>
2023-12-26 13:40:02 +0530

    Added Buttons emptying the given task list and displaying empty message

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import "./styles.css";
 import Header from "./Header/Header.js";
 import ToDoReturn from "./List/ToDoReturn.js";
-
+import { useState } from "react";
 
 export default function App() {
-  const tasks=["Read SpringBoot", "Complete assignments", "Prepare breakfast", "Sleep for 2 hours", "Take a shower"];
+  const [tasks,setTasks]=useState(["Read SpringBoot", "Complete assignments", "Prepare breakfast", "Sleep for 2 hours", "Take a shower"]);
   return (
     <div className="App">
       <Header />
       <hr />
-    
-    {tasks.map((task,index)=>(<div key={index}>
+    {tasks?
+    (
+    tasks.map((task,index)=>(<div key={index}>
       <ToDoReturn value={task}/>
     </div>
-    ))}
+    ))
+    ):(<h3 className="Header">"Nothing to do buddy. Sleep!"</h3>)}
+    <div className="Header">
+    <button onClick={()=>{
+      setTasks(null);
+    }} >Empty</button>
+    </div>
     
     </div>
   );
