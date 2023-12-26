c07ae5fbaaf8a33f02573271a49b612241b50c5b
vineeth <vineethmvinoy@gmail.com>
2023-12-26 17:28:30 +0530

    Finished To Do List with comments

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 
 export default function App() {
-  const [tasks, setTasks] = useState([
+  const [tasks, setTasks] = useState([                     //Setting states to have the name and its status of completion in state assignment
     { name: "Read SpringBoot", completed: false },
     { name: "Complete assignments", completed: false },
     { name: "Prepare breakfast", completed: false },
@@ -16,17 +16,17 @@ export default function App() {
     setTasks(tasks => tasks.filter(task => !task.completed));
     console.log(tasks);
   }
-  function ToDoReturn({id,task}){
-    const [taskDone,setTaskDone]=useState(task.completed);
+  function ToDoReturn({id,task}){                                //Function That mainly return tasks as buttons & updates the state of tasks as completed 
+    const [taskDone,setTaskDone]=useState(task.completed);      //or not ,also  updates text to have the line through or not.
     const styles = {
       textDecoration: taskDone ? 'line-through' : 'none',
       background: 'none',
       border: 'none',
       cursor: 'pointer', 
     };
-    function handleClick(){
-      setTaskDone(!taskDone);
-      const tempTasks=tasks.slice();
+    function handleClick(){                //We use two states one having the whole name and completed steps other only having the completed values
+      setTaskDone(!taskDone);             //Using the both states we find the tasks that has been checked when pressed and updates its state in both  
+      const tempTasks=tasks.slice();     //places
       for(let i=0;i<tempTasks.length;i++)
       {
         if(tempTasks[i].name===task.name)
@@ -35,14 +35,14 @@ export default function App() {
       setTasks(tempTasks);
     }
     return (
-      <button id={id} style={styles}  onClick={handleClick}>{task.name}</button>
+      <button id={id} style={styles}  onClick={handleClick}>{task.name}</button> //returning buttons
      );
   }
   return (
     <div className="App">
       <Header />
       <hr />
-    <div className="Header">{tasks.length!=0?
+    <div className="Header">{tasks.length!=0?                    //Condition to check if tasks list is empty or not if empty display new text
     (
     tasks.map((task,index)=>(<div key={index}>
       <ToDoReturn key={index} id={index} task={task}/>
